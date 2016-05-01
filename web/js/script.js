74aed465c619c734b632e6f93fa4cdf7f2cde237
Rok Andrée <rok@andree.si>
2016-05-01 17:10:28 +0200

    Adds stats subpage

--- web/js/script.js
+++ web/js/script.js
@@ -1,23 +1,55 @@
 (function($){
   $(function(){
 
-  	//
-  	// Materializecss
-  	// 
+    //
+    // Materializecss
+    // 
     $('.button-collapse').sideNav();
-  	
-  	// 
-  	// Get list of domains
-  	// 
-  	var apiPath = config.apiDomain + '/api/v1/domain'; 
-  	params = {
-  		access_code: config.accessCode,
-  	};
-  	$.get(apiPath, params, function(data) {
-  		data.forEach(function(element) {
-  			$('select#domain').append('<option value="'+element+'">'+element+'</option>')
-  		});
-  	});
+    
+    // 
+    // Get list of domains
+    // 
+    var apiPath = config.apiDomain + '/api/v1/domain'; 
+    params = {
+      access_code: config.accessCode,
+    };
+
+    if ($('div.stats-tabs').length) {
+      var domains = [];
+      $.get(apiPath, params, function(data) {
+        data.forEach(function(element) {
+          var slug = element.replace('.', '');
+          $('ul.tabs').append('<li class="tab col s3"><a href="#'+slug+'">'+element+'</a></li>');
+          $('div.stats-tabs').append('<div id="'+slug+'" class="col s12"></div>');
+          $('ul.tabs').tabs();
+          domains.push(element);
+
+          $.get(config.apiDomain + '/api/v1/url/' + element, function(data) {
+            var table = '<table><thead><tr><th data-field="short">Short URL</th><th data-field="target">Target URL</th><th data-field="stats" class="center-align">Stats <br /><small>today | 7 days | 30 days | alltime</small></th><th data-field="date">Created at</th></tr></thead>';
+            table = table + '<tbody>';
+
+            data.forEach(function(s) {
+              var stats = s.stats.hits_today + " | " + s.stats.hits_7days + " | " + s.stats.hits_30days + " | " + s.stats.hits_all;
+              table = table + '<tr><td>'+s.shortUrl+'</td><td class="truncate table-target-url"><a href="'+s.targetUrl+'">'+s.targetUrl+'</a></td><td class="center-align">'+stats+'</td><td>'+new Date(s.timestamp * 1000).toDateString()+'</td></tr>'
+            });
+
+            table = table + '</tbody></table>';
+            $('#'+slug).append(table);
+          });
+        });
+
+      });
+
+
+      
+
+    } else {
+      $.get(apiPath, params, function(data) {
+        data.forEach(function(element) {
+          $('select#domain').append('<option value="'+element+'">'+element+'</option>')
+        });
+      });
+    }
 
   	//
   	// Form ADD submit handler
